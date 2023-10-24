651db58d5f700745ee36df6b37cb6b3ef7a76132
frankiwi6x6 <106415042+frankiwi6x6@users.noreply.github.com>
2023-10-23 21:34:06 -0300

    Delete apirestdjangocrud directory

--- apirestdjangocrud/crudrest/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrudrestConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crudrest'
--- apirestdjangocrud/web/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class ProductoForm(forms.Form):
-    nombre = forms.CharField(max_length=100)
-    precio = forms.CharField(max_length=100)
-    stock = forms.CharField(max_length=100)

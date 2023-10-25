03914c1472adf30a0082cdb17fad30a9ada6116d
Vina del Mar <vina@duoc.acad>
2023-10-25 20:51:06 -0300

    Added requirements.txt

--- API_calificacion/API_calificacion/settings.py
+++ API_calificacion/API_calificacion/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-!bknv0qv^(lp%sihdw2j(!^f7d^cfbvdxdk0u=-x_k8i7dulj@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1']
 
 
 # Application definition

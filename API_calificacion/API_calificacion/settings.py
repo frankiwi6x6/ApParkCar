5c84b305af700233d82fe5a29f4555088b87acf2
Vina del Mar <vina@duoc.acad>
2023-10-25 21:50:36 -0300

    Correción

--- API_calificacion/API_calificacion/settings.py
+++ API_calificacion/API_calificacion/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

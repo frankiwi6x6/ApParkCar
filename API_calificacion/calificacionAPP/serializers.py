8227662399797a93d88b3bcb7b6caf1f1f2bc8c5
KernelOS <fra.reyesj@duocuc.cl>
2023-11-06 13:16:43 -0300

    added docker compose

--- API_calificacion/calificacionAPP/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import calificacionUsuario
-
-
-class CalificacionUsuariosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = calificacionUsuario
-        fields = '__all__'
